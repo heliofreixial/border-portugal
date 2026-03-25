6e3ee271382c394acc359fb842af6f97d0570bb1
heliofreixial <helio.freixial@protonmail.com>
2026-03-25 01:13:49 +0000

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,22 @@
+var CACHE = 'border-lisboa-v1';
+var FILES = ['.', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
+
+self.addEventListener('install', function(e){
+  e.waitUntil(caches.open(CACHE).then(function(c){ return c.addAll(FILES); }));
+  self.skipWaiting();
+});
+self.addEventListener('activate', function(e){
+  e.waitUntil(caches.keys().then(function(keys){
+    return Promise.all(keys.filter(function(k){ return k!==CACHE; }).map(function(k){ return caches.delete(k); }));
+  }));
+  self.clients.claim();
+});
+self.addEventListener('fetch', function(e){
+  e.respondWith(
+    caches.match(e.request).then(function(r){
+      return r || fetch(e.request).then(function(resp){
+        return caches.open(CACHE).then(function(c){ c.put(e.request,resp.clone()); return resp; });
+      });
+    }).catch(function(){ return caches.match('./index.html'); })
+  );
+});
